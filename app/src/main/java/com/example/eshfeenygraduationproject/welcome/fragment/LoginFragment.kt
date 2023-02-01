010d43ba43c63f908a6cae4a77f756810229ebc1
youssef mohammed <youssef.mu.saber@gmail.com>
2023-02-01 23:03:00 +0200

    fixed some issues with login and signup passwords text views

--- app/src/main/java/com/example/eshfeenygraduationproject/welcome/fragment/LoginFragment.kt
+++ app/src/main/java/com/example/eshfeenygraduationproject/welcome/fragment/LoginFragment.kt
@@ -1,27 +1,76 @@
 package com.example.eshfeenygraduationproject.welcome.fragment
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.InputType
+import android.text.TextWatcher
+import android.util.Log
+import android.view.Gravity
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
+import android.view.View.*
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
 import com.example.eshfeenygraduationproject.R
 import com.example.eshfeenygraduationproject.databinding.FragmentLoginBinding
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 
 
 class LoginFragment : Fragment() {
     private lateinit var binding: FragmentLoginBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         binding = FragmentLoginBinding.inflate(inflater)
+
+        binding.passwordSignup.hint = getString(R.string.pass)
+        binding.nameSignin.hint = getString(R.string.Email)
+
+        setHint(binding.nameSignin)
+        setHint(binding.passwordSignup, binding.PasswordSignup)
+
         binding.createInSignin.setOnClickListener {
             Navigation.findNavController(it).navigate(R.id.action_loginFragmen_to_signupFragment)
         }
         return binding.root
+    }
+
+    fun setHint(view: TextInputEditText, parent: TextInputLayout) {
+        view.setOnFocusChangeListener { v, hasFocus ->
+            if (hasFocus) {
+                parent.setEndIconMode(TextInputLayout.END_ICON_PASSWORD_TOGGLE)
+                view.hint = ""
+                (v as TextInputEditText).gravity = Gravity.LEFT
+            } else {
+                parent.setEndIconMode(TextInputLayout.END_ICON_NONE)
+                if (view.text?.isEmpty() == true) {
+                    view.hint = getString(R.string.pass)
+                    (v as TextInputEditText).gravity = Gravity.RIGHT
+                }
+            }
+        }
+    }
+
+    fun setHint(view: TextInputEditText) {
+        view.setOnFocusChangeListener { v, hasFocus ->
+            if (hasFocus) {
+                view.hint = ""
+                (v as TextInputEditText).gravity = Gravity.LEFT
+            } else {
+                if (view.text?.isEmpty() == true) {
+                    view.hint = getString(R.string.Email)
+                    (v as TextInputEditText).gravity = Gravity.RIGHT
+                }
+            }
+        }
     }
 
 }
--- app/src/main/java/com/example/eshfeenygraduationproject/welcome/fragment/SignupFragment.kt
+++ app/src/main/java/com/example/eshfeenygraduationproject/welcome/fragment/SignupFragment.kt
@@ -1,6 +1,7 @@
 package com.example.eshfeenygraduationproject.welcome.fragment
 
 import android.os.Bundle
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,6 +9,8 @@ import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
 import com.example.eshfeenygraduationproject.R
 import com.example.eshfeenygraduationproject.databinding.FragmentSignupBinding
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 
 
 class SignupFragment : Fragment() {
@@ -18,6 +21,12 @@ class SignupFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         binding = FragmentSignupBinding.inflate(inflater)
+
+        setHint(binding.nameSignup, R.string.userName)
+        setHint(binding.emailSignup, R.string.Email)
+        setHint(binding.passwordSignup, binding.PasswordSignup, R.string.pass)
+        setHint(binding.confirmPassSignup, binding.ConfirmPassSignup, R.string.confPass)
+
         binding.createInSignup.setOnClickListener{
             //action from signup to login
             Navigation.findNavController(it).navigate(R.id.action_signupFragment_to_loginFragmen)
@@ -25,4 +34,39 @@ class SignupFragment : Fragment() {
         return binding.root
     }
 
+    fun setHint(view: TextInputEditText, parent: TextInputLayout, string: Int) {
+
+        view.hint = getString(string)
+
+        view.setOnFocusChangeListener { v, hasFocus ->
+            if (hasFocus) {
+                parent.setEndIconMode(TextInputLayout.END_ICON_PASSWORD_TOGGLE)
+                view.hint = ""
+                (v as TextInputEditText).gravity = Gravity.LEFT
+            } else {
+                parent.setEndIconMode(TextInputLayout.END_ICON_NONE)
+                if (view.text?.isEmpty() == true) {
+                    view.hint = getString(string)
+                    (v as TextInputEditText).gravity = Gravity.RIGHT
+                }
+            }
+        }
+    }
+
+    fun setHint(view: TextInputEditText, string: Int) {
+
+        view.hint = getString(string)
+
+        view.setOnFocusChangeListener { v, hasFocus ->
+            if (hasFocus) {
+                view.hint = ""
+                (v as TextInputEditText).gravity = Gravity.LEFT
+            } else {
+                if (view.text?.isEmpty() == true) {
+                    view.hint = getString(string)
+                    (v as TextInputEditText).gravity = Gravity.RIGHT
+                }
+            }
+        }
+    }
 }
