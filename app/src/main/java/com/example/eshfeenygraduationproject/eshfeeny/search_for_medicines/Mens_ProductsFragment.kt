5424c04c86dd8a7b81e917ac2fd8a4b99dc1eef3
Mohamed Ibrahim <mohamedibra3232@gmail.com>
2023-03-29 01:21:07 +0200

    add Men's protection Fragment and xml file

---
+++ app/src/main/java/com/example/eshfeenygraduationproject/eshfeeny/search_for_medicines/Mens_ProductsFragment.kt
@@ -0,0 +1,23 @@
+package com.example.eshfeenygraduationproject.eshfeeny.search_for_medicines
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.eshfeenygraduationproject.R
+import com.example.eshfeenygraduationproject.databinding.FragmentMensProductsBinding
+
+
+class Mens_ProductsFragment : Fragment() {
+    private var binding:FragmentMensProductsBinding? = null
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentMensProductsBinding.inflate(layoutInflater)
+        // Inflate the layout for this fragment
+        return binding?.root
+    }
+
+}
