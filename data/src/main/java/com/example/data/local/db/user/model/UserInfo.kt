bac30c909f31aa66fa84bf2b866f5cdb1819a367
Youssef Mohammed <youssef.mu.saber@gmail.com>
2023-03-29 01:50:52 +0200

    changed the UserInfo table parameters

--- data/src/main/java/com/example/data/local/db/user/model/UserInfo.kt
+++ data/src/main/java/com/example/data/local/db/user/model/UserInfo.kt
@@ -2,12 +2,18 @@ package com.example.data.local.db.user.model
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.example.domain.entity.InsuranceCardX
 
 @Entity("user_details")
 data class UserInfo(
     @PrimaryKey(autoGenerate = true)
     val id: Int,
-    val _ID: String,
-    val userEmail: String,
-    val userPassword: String
+    val _id: String,
+    val name: String,
+    val password: String,
+    val email: String,
+    val age: String,
+    val gender: String,
+    val phoneNumber: String,
+    val insuranceCards: List<InsuranceCardX>
 )
