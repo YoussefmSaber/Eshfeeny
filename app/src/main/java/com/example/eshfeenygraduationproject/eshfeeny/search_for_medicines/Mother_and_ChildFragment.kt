c877608f0fb2a4a890aae7c8761515a2156f4fd4
Mohamed Ibrahim <mohamedibra3232@gmail.com>
2023-03-29 01:08:14 +0200

    add Mother and child protection Fragment and xml file

---
+++ app/src/main/java/com/example/eshfeenygraduationproject/eshfeeny/search_for_medicines/Mother_and_ChildFragment.kt
@@ -0,0 +1,22 @@
+package com.example.eshfeenygraduationproject.eshfeeny.search_for_medicines
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.eshfeenygraduationproject.R
+import com.example.eshfeenygraduationproject.databinding.FragmentMotherAndChildBinding
+
+
+class Mother_and_ChildFragment : Fragment() {
+    private var binding:FragmentMotherAndChildBinding? = null
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentMotherAndChildBinding.inflate(layoutInflater)
+        // Inflate the layout for this fragment
+        return binding?.root
+    }
+}
