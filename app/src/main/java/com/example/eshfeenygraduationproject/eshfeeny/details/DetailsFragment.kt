a5743b7d004ecf6e046eadc23f5b45c370de81ba
Mohamed Ibrahim <mohamedibra3232@gmail.com>
2023-04-12 01:15:39 +0200

    Fixed error in Xml Details and add Igame Url file

--- app/src/main/java/com/example/eshfeenygraduationproject/eshfeeny/details/DetailsFragment.kt
+++ app/src/main/java/com/example/eshfeenygraduationproject/eshfeeny/details/DetailsFragment.kt
@@ -1,12 +1,11 @@
 package com.example.eshfeenygraduationproject.eshfeeny.details
 
+
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.viewModels
-import com.example.eshfeenygraduationproject.R
+import androidx.fragment.app.Fragment
 import com.example.eshfeenygraduationproject.databinding.FragmentDetailsBinding
 import com.example.eshfeenygraduationproject.eshfeeny.medicine.SideEffectsAdapter
 import com.example.eshfeenygraduationproject.eshfeeny.medicine.UsageAdapter
