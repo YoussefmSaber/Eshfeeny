0b2a21cc60af20b84f4d4c455a6772660627301c
Mohamed Ibrahim <mohamedibra3232@gmail.com>
2023-04-20 16:09:28 +0200

    add bottom sheet in verify data fragment

--- app/src/main/java/com/example/eshfeenygraduationproject/eshfeeny/bmiandbmr/VerifyDataFragment.kt
+++ app/src/main/java/com/example/eshfeenygraduationproject/eshfeeny/bmiandbmr/VerifyDataFragment.kt
@@ -5,11 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
+import com.example.eshfeenygraduationproject.R
 import com.example.eshfeenygraduationproject.databinding.FragmentVerifyDataBinding
+
+import com.google.android.material.bottomsheet.BottomSheetDialog
 
 
 class VerifyDataFragment : Fragment() {
-    private var binding:FragmentVerifyDataBinding? = null
+    private var binding: FragmentVerifyDataBinding? = null
+    private lateinit var navController: NavController
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -19,12 +26,38 @@ class VerifyDataFragment : Fragment() {
         val height = arguments?.getInt("Height")
         val weight = arguments?.getInt("Weight")
         val gender = arguments?.getString("Gender")
-        binding?.txtAgeId?.text = "$age"
-        binding?.txtCmId?.text = "$height"
-        binding?.txtKgId?.text = "$weight"
-
+        binding?.txtAgeId?.text = "$age age"
+        binding?.txtCmId?.text = "$height cm"
+        binding?.txtKgId?.text = "$weight kg"
+        val height1 = height?.toFloat()
+        val weight1 = weight?.toFloat()
+        if(gender == "أنثى") {
+            binding?.imgManId?.visibility = View.INVISIBLE
+            binding?.imgWomanId?.visibility = View.VISIBLE
+        }
+//        binding?.btnEdit?.setOnClickListener {
+//            navController.navigate(R.id.action_verifyDataFragment_to_bmiAndBmrFragment)
+//        }
+        binding?.btnCal?.setOnClickListener {
+            val bmr = calcBMR(age?.toFloat(),height1,weight1,gender)
+            val bmi = calcBMI(height1,weight1)
+            val view: View = layoutInflater.inflate(R.layout.bottom_sheet_to_calculate_bmi_bmr, null)
+            val dialog = BottomSheetDialog(requireContext())
+            val BtnIdBmi = view.findViewById<Button>(R.id.btnIdCalcBmi)
+            val BtnIdBmr = view.findViewById<Button>(R.id.btnIdCalcBmr)
+            dialog.setContentView(view)
+            dialog.show()
+        }
         // Inflate the layout for this fragment
         return binding?.root
     }
-
+    fun calcBMR(age: Float?, height: Float?, weight: Float?, gender: String?) : Double {
+        return if(gender != "أنثى")
+            88.362 + (13.397 * weight!!) + (4.799 * height!!) - (5.677 * age!!)
+        else
+            447.593  + (9.247 * weight!!) + (3.098 * height!!) - (4.330 * age!! )
+    }
+    fun calcBMI(height: Float?, weight: Float?) : Float {
+        return (weight!!/((height!!/100)*(height /100)))
+    }
 }
