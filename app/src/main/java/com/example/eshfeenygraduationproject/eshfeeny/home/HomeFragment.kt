15868888d8f67aef74ce81f87a2e08afa3b6b3c4
Mohamed Ibrahim <mohamedibra3232@gmail.com>
2023-04-29 23:29:02 +0300

    make navigate from home to roshta

--- app/src/main/java/com/example/eshfeenygraduationproject/eshfeeny/home/HomeFragment.kt
+++ app/src/main/java/com/example/eshfeenygraduationproject/eshfeeny/home/HomeFragment.kt
@@ -6,9 +6,11 @@ import androidx.fragment.app.Fragment
 import android.view.*
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.Navigation
+import androidx.navigation.fragment.findNavController
 import com.denzcoskun.imageslider.constants.*
 import com.denzcoskun.imageslider.models.SlideModel
 import com.example.data.repository.ProductRepoImpl
+import com.example.eshfeenygraduationproject.R
 import com.example.eshfeenygraduationproject.databinding.FragmentHomeBinding
 import com.example.eshfeenygraduationproject.eshfeeny.productsAdapter.ProductHomeAdapter
 import com.example.eshfeenygraduationproject.eshfeeny.searchForProducts.*
@@ -134,6 +136,9 @@ class HomeFragment : Fragment() {
                     }
                 }
             }
+        }
+        binding?.addRoshtaPhotoId?.setOnClickListener {
+            findNavController().navigate(R.id.action_homeFragment2_to_roshtaFragment)
         }
 
         //call recycler view for امساك
