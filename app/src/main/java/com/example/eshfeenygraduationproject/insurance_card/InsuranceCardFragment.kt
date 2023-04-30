3825d81731c63d4d1775dab522eec5c7194a55be
Mohamed Ibrahim <mohamedibra3232@gmail.com>
2023-04-30 20:04:32 +0300

    Add Insurance card Fragment

---
+++ app/src/main/java/com/example/eshfeenygraduationproject/insurance_card/InsuranceCardFragment.kt
@@ -0,0 +1,24 @@
+package com.example.eshfeenygraduationproject.insurance_card
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.eshfeenygraduationproject.R
+import com.example.eshfeenygraduationproject.databinding.FragmentInsuranceCardBinding
+
+
+class InsuranceCardFragment : Fragment() {
+    private var binding:FragmentInsuranceCardBinding? = null
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentInsuranceCardBinding.inflate(layoutInflater)
+
+
+        // Inflate the layout for this fragment
+        return binding?.root
+    }
+}
