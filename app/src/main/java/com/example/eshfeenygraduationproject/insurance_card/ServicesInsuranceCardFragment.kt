dd2842ac955efccc55d16ad9b108c8d5a1d2c07e
Mohamed Ibrahim <mohamedibra3232@gmail.com>
2023-05-01 16:16:35 +0300

    Some edit in insurance card fragment and nav

--- app/src/main/java/com/example/eshfeenygraduationproject/insurance_card/ServicesInsuranceCardFragment.kt
+++ app/src/main/java/com/example/eshfeenygraduationproject/insurance_card/ServicesInsuranceCardFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.eshfeenygraduationproject.R
 import com.example.eshfeenygraduationproject.databinding.FragmentServicesInsuranceCardBinding
 
@@ -16,6 +17,19 @@ class ServicesInsuranceCardFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentServicesInsuranceCardBinding.inflate(layoutInflater)
+        var num = arguments?.getInt("num")
+        when (num) {
+            1 -> binding?.TVId?.text = "EGYCARE"
+            2 -> binding?.TVId?.text = "MetLife"
+            3 -> binding?.TVId?.text = "Misr"
+            else -> binding?.TVId?.text = "${num.toString()}"
+        }
+
+
+
+        binding?.backBtn22?.setOnClickListener {
+            requireActivity().supportFragmentManager.popBackStack()
+        }
         // Inflate the layout for this fragment
         return binding?.root
     }
