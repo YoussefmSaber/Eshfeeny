4f15edee0c245a7dc58bfea9347b9f171e30d731
Mohamed Ibrahim <mohamedibra3232@gmail.com>
2023-05-02 16:06:25 +0300

    Finish info insurance card xml

---
+++ app/src/main/java/com/example/eshfeenygraduationproject/eshfeeny/insurance_card/InfoInsuranceCardFragment.kt
@@ -0,0 +1,24 @@
+package com.example.eshfeenygraduationproject.eshfeeny.insurance_card
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.eshfeenygraduationproject.R
+import com.example.eshfeenygraduationproject.databinding.FragmentInfoInsuranceCardBinding
+
+
+class InfoInsuranceCardFragment : Fragment() {
+    private var binding:FragmentInfoInsuranceCardBinding? = null
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentInfoInsuranceCardBinding.inflate(layoutInflater)
+
+
+        // Inflate the layout for this fragment
+        return binding?.root
+    }
+}
