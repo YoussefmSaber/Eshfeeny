68ab918bbd6fb2e86822837b0464832942e73cf9
Youssef Mohammed <youssef.mu.saber@gmail.com>
2023-05-08 02:23:32 +0300

    Added the recycler view adapter and the data is now desplayed

--- app/src/main/java/com/example/eshfeenygraduationproject/eshfeeny/brands/BrandsFragment.kt
+++ app/src/main/java/com/example/eshfeenygraduationproject/eshfeeny/brands/BrandsFragment.kt
@@ -5,21 +5,34 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.eshfeenygraduationproject.R
 import com.example.eshfeenygraduationproject.databinding.FragmentBrandsBinding
+import com.example.eshfeenygraduationproject.eshfeeny.productsAdapter.BrandsAdapter
+import com.example.eshfeenygraduationproject.eshfeeny.util.BrandsImages
 
 
 class BrandsFragment : Fragment() {
 
+    private var binding: FragmentBrandsBinding? = null
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_brands, container, false)
+        binding = FragmentBrandsBinding.inflate(layoutInflater)
+
+        binding?.backButtonBrand?.setOnClickListener {
+            findNavController().navigate(R.id.action_brandsFragment_to_homeFragment2)
+        }
+
+        val adapter = BrandsAdapter(BrandsImages.brands)
+        binding?.brandsRecyclerView?.adapter = adapter
+
+        return binding?.root
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
+        binding = null
     }
 
 }
