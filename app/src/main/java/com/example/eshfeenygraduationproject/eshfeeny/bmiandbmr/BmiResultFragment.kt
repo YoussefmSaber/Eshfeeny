e3516014dba9b085656bd86c4967607203f12750
Mohamed Ibrahim <mohamedibra3232@gmail.com>
2023-05-11 00:53:53 +0300

    Make bmi result xml

--- app/src/main/java/com/example/eshfeenygraduationproject/eshfeeny/bmiandbmr/BmiResultFragment.kt
+++ app/src/main/java/com/example/eshfeenygraduationproject/eshfeeny/bmiandbmr/BmiResultFragment.kt
@@ -6,14 +6,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.eshfeenygraduationproject.R
+import com.example.eshfeenygraduationproject.databinding.FragmentBmiResultBinding
 
 
 class BmiResultFragment : Fragment() {
+    private var binding : FragmentBmiResultBinding? = null
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        binding = FragmentBmiResultBinding.inflate(layoutInflater)
+
+
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_bmi_result, container, false)
+        return binding?.root
     }
 }
