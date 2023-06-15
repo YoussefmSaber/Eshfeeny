4d93f88ae7dcda76bd5fa45f626a5d3daccc0cf3
Youssef <youssef.mu.saber@gmail.com>
2023-06-15 21:45:45 +0300

    Added the binding variable to the fragment kotlin file

--- app/src/main/java/com/example/eshfeenygraduationproject/eshfeeny/more/LogoutBottomSheetFragment.kt
+++ app/src/main/java/com/example/eshfeenygraduationproject/eshfeeny/more/LogoutBottomSheetFragment.kt
@@ -6,55 +6,26 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.eshfeenygraduationproject.R
+import com.example.eshfeenygraduationproject.databinding.FragmentLogoutBottomSheetBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [LogoutBottomSheetFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class LogoutBottomSheetFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private var binding: FragmentLogoutBottomSheetBinding? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+        binding = FragmentLogoutBottomSheetBinding.inflate(inflater)
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_logout_bottom_sheet, container, false)
+        return binding?.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment LogoutBottomSheetFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            LogoutBottomSheetFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onDestroyView() {
+        super.onDestroyView()
+        binding = null
     }
 }
