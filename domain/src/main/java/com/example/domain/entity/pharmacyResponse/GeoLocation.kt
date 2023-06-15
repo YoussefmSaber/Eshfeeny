9f2ca393cce33b2d2f7bde4de484c8c1a7464d8f
Youssef <youssef.mu.saber@gmail.com>
2023-06-15 10:56:19 +0300

    Created the pharmacyResponse DataClass

---
+++ domain/src/main/java/com/example/domain/entity/pharmacyResponse/GeoLocation.kt
@@ -0,0 +1,6 @@
+package com.example.domain.entity.pharmacyResponse
+
+data class GeoLocation(
+    val lat: Double,
+    val lng: Double
+)
---
+++ domain/src/main/java/com/example/domain/entity/pharmacyResponse/PharmacyResponseItem.kt
@@ -0,0 +1,14 @@
+package com.example.domain.entity.pharmacyResponse
+
+data class PharmacyResponseItem(
+    val _id: String,
+    val address: String,
+    val cart: List<Cart>,
+    val email: String,
+    val favorites: List<String>,
+    val geoLocation: GeoLocation,
+    val name: String,
+    val password: String,
+    val phoneNumber: String,
+    val products: List<ProductX>
+)
