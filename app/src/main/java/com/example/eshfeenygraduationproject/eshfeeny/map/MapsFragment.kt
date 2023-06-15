1ff7ee3e34300138707e286566ca0c674c90caed
Youssef <youssef.mu.saber@gmail.com>
2023-06-15 11:29:29 +0300

    Showing all the pharmacies on the map

--- app/src/main/java/com/example/eshfeenygraduationproject/eshfeeny/map/MapsFragment.kt
+++ app/src/main/java/com/example/eshfeenygraduationproject/eshfeeny/map/MapsFragment.kt
@@ -11,8 +11,12 @@ import android.view.ViewGroup
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.data.repository.PharmacyRepoImpl
 import com.example.eshfeenygraduationproject.R
 import com.example.eshfeenygraduationproject.databinding.FragmentMapsBinding
+import com.example.eshfeenygraduationproject.eshfeeny.publicViewModel.viewModel.PharmacyViewModel
+import com.example.eshfeenygraduationproject.eshfeeny.publicViewModel.viewModelFactory.PharmacyViewModelFactory
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
@@ -28,20 +32,7 @@ class MapsFragment : Fragment() {
     private var binding: FragmentMapsBinding? = null
     private val requestLocationPermission = 1
     private lateinit var googleMap: GoogleMap // declare the variable for the GoogleMap object
-
-    private val callback = OnMapReadyCallback { map ->
-        googleMap = map // assign the map to the variable
-        val sydney = LatLng(30.591363051596, 32.268719749118574)
-        googleMap.addMarker(
-            MarkerOptions()
-                .position(sydney)
-                .title("Marker in Sydney")
-                .icon(BitmapDescriptorFactory.fromResource(R.drawable.pharmacy_pin))
-        )
-        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 18f))
-        setMapStyle(googleMap)
-        enableMyLocation(googleMap)
-    }
+    private lateinit var viewModel: PharmacyViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -49,6 +40,11 @@ class MapsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentMapsBinding.inflate(inflater)
+
+        val repo = PharmacyRepoImpl()
+        val viewModelFactory = PharmacyViewModelFactory(repo)
+        viewModel = ViewModelProvider(this, viewModelFactory)[PharmacyViewModel::class.java]
+
         return binding?.root
     }
 
@@ -56,6 +52,24 @@ class MapsFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
         mapFragment?.getMapAsync(callback)
+    }
+
+    private val callback = OnMapReadyCallback { map ->
+        googleMap = map // assign the map to the variable
+
+        viewModel.getAllPharmacies()
+        viewModel.allPharmacies.observe(viewLifecycleOwner) { pharmacyResponse ->
+            pharmacyResponse.forEach {
+                googleMap.addMarker(
+                    MarkerOptions()
+                        .position(LatLng(it.geoLocation.lat, it.geoLocation.lng))
+                        .title(it.name)
+                        .icon(BitmapDescriptorFactory.fromResource(R.drawable.pharmacy_pin))
+                )
+            }
+        }
+        setMapStyle(googleMap)
+        enableMyLocation(googleMap)
     }
 
     override fun onDestroyView() {
--- app/src/main/java/com/example/eshfeenygraduationproject/eshfeeny/publicViewModel/viewModelFactory/PharmacyViewModelFactory.kt
+++ app/src/main/java/com/example/eshfeenygraduationproject/eshfeeny/publicViewModel/viewModelFactory/PharmacyViewModelFactory.kt
@@ -1,4 +1,12 @@
 package com.example.eshfeenygraduationproject.eshfeeny.publicViewModel.viewModelFactory
 
-class PharmacyViewModelFactory {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.data.repository.PharmacyRepoImpl
+import com.example.eshfeenygraduationproject.eshfeeny.publicViewModel.viewModel.PharmacyViewModel
+
+class PharmacyViewModelFactory(private val repository: PharmacyRepoImpl): ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        return PharmacyViewModel(repository) as T
+    }
 }
